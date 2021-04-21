4a057fec1edc068a0857dcce0e00e5c67a29fe90
Daniel <dan_larby@hotmail.co.uk>
2021-04-21 23:14:42 +0100

    booooooooooooooooooooooooooosted

--- AntGame/AntGame/main.cpp
+++ AntGame/AntGame/main.cpp
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <iostream>
 #include "foo.h"
 #include "bar.h"
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+using namespace boost::geometry;
+
 
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
-    {
-        bar ting;
-    }
+    foo();
+    
+    model::d2::point_xy<int> p1(1, 1), p2(2, 2);
+    std::cout << "Distance p1-p2 is: " << distance(p1, p2) << std::endl;
+
 	return 0;
 }
